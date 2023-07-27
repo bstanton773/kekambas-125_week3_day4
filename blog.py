36b940bb0f45c7d1ef7f977850a5a985ff986dc1
Brian <bstanton773@gmail.com>
2023-07-27 14:11:10 -0500

    Add login functionality to the blog

--- blog.py
+++ blog.py
@@ -3,6 +3,7 @@ class Blog:
     def __init__(self):
         self.users = []
         self.posts = []
+        self.current_user = None
 
     # Method to create a new user instance and add to the Blog's users
     def create_new_user(self):
@@ -18,6 +19,24 @@ class Blog:
             # Add the new user to the users list
             self.users.append(new_user)
             print(f"{new_user} has been created")
+
+    # Method will log a user in by setting the current_user to a User instance
+    def log_user_in(self):
+        # Get user credentials via input
+        username = input('What is your username? ')
+        password = input('What is your password? ')
+        # Loop through each user in the blog
+        for user in self.users:
+            # Check if the user has that username AND the user's password is correct
+            if user.username == username and user.check_password(password):
+                # If credentials are correct, set the blog's current_user to that user instance
+                self.current_user = user
+                print(f"{user} has logged in")
+                break
+        # If no users in the blog have that username/password, flash invalid credentials message
+        else:
+            print("Username and/or password is incorrect")
+
 
 
 class User:
@@ -35,6 +54,9 @@ class User:
     def __repr__(self):
         return f"<User {self.id}|{self.username}>"
 
+    def check_password(self, password_attempt):
+        return self.password == hash(password_attempt)
+
 class Post:
     pass
 
@@ -46,21 +68,34 @@ def run_blog():
     my_blog = Blog()
     # Start "running" the blog until the user quits
     while True:
-        # Print our menu options
-        print("1. Sign Up\n5. Quit")
-        # Ask the user which option they would like to do
-        to_do = input('Which option would you like to do? ')
-        # Keep asking is user does not choose a valid option
-        while to_do not in {'1', '5'}:
-            to_do = input('Invalid option. Please choose 1 or 5. ')
-        # If the user chooses 5, quit the program
-        if to_do == '5':
-            print('Thanks for checking out the blog!')
-            break
-        # If they choose option 1, sign the user up
-        elif to_do == '1':
-            # Call the Blog method to add a new user
-            my_blog.create_new_user()
+        # If there is currently nobody logged in to the blog (aka the current_user is None)
+        if my_blog.current_user is None:
+            # Print our menu options
+            print("1. Sign Up\n2. Log In\n5. Quit")
+            # Ask the user which option they would like to do
+            to_do = input('Which option would you like to do? ')
+            # Keep asking is user does not choose a valid option
+            while to_do not in {'1', '5', '2'}:
+                to_do = input('Invalid option. Please choose 1, 2, or 5. ')
+            # If the user chooses 5, quit the program
+            if to_do == '5':
+                print('Thanks for checking out the blog!')
+                break
+            # If they choose option 1, sign the user up
+            elif to_do == '1':
+                # Call the Blog method to add a new user
+                my_blog.create_new_user()
+            # If they choose option 2, log the user in
+            elif to_do == '2':
+                # Call the blog method to log user in
+                my_blog.log_user_in()
+        # If there is a logged in user (aka current_user is not None, it is a user instance)
+        else:
+            # Ask the user for an input and then quit.
+            print('Quit')
+            to_do = input('Which option would you like to do? ')
+            if to_do == 'quit':
+                break
 
 
 # Call the function to actually start the blog
